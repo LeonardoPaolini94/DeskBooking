cdb327a56de087d54cf2d36f639b049d7adfeee0
Gabriele Marabitti <lele@MacBook-Pro-di-Gabriele.local>
2022-07-21 10:46:37 +0200

    date complete

--- src/app/core/service/room-status-service/room-status.service.ts
+++ src/app/core/service/room-status-service/room-status.service.ts
@@ -10,7 +10,7 @@ export class RoomStatusService {
 
   constructor(private http : HttpClient) { }
 
-  getAllRoomStatus() : Observable<RoomStatus[]> {
+  getAllRoomStatus(date : String) : Observable<RoomStatus[]> {
     return this.http.get<RoomStatus[]>("http://localhost:3000/roomstatus")
   }
 
