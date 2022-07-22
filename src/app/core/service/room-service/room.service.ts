3b974c125e11f489832bf567a214dac051d413ff
federicodecaro <federicodcr96@outlook.it>
2022-07-22 13:04:20 +0200

    merge fixes

--- src/app/core/service/room-service/room.service.ts
+++ src/app/core/service/room-service/room.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Room} from "../../models/Room";
+import {RoomStatus} from "../../models/RoomStatus";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,10 @@ export class RoomService {
 
   constructor(private http : HttpClient) { }
 
+  getAllRooms() : Observable<RoomStatus[]>{
+    return this.http.get<RoomStatus[]>("http://localhost:8080/api/v1/room")
+  }
   getRoomByRoomNumber(roomNumber : number) : Observable<Room> {
-    return this.http.get<Room>("http://localhost:3000/rooms/" + roomNumber)
+    return this.http.get<Room>("http://localhost:8080/api/v1/room/roomnumber/" + roomNumber)
   }
 }
