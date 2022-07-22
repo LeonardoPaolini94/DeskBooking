91b23d9e723bce4a56a669d6006b0c8b4ab0f684
federicodecaro <federicodcr96@outlook.it>
2022-07-22 12:44:40 +0200

    merge fixes

--- src/app/core/service/booking.service.ts
+++ src/app/core/service/booking.service.ts
@@ -23,10 +23,7 @@ export class BookingService {
   }
 
   deleteBookingById(id : number | undefined) : Observable<Booking>{
-    return this.httpClient.delete<Booking>("http://localhost:3000/Bookings/" + id);
+    return this.httpClient.delete<Booking>("http://localhost:8080/api/v1/booking/" + id);
   }
 
-  deleteBookingById(id: number | undefined) : Observable<Booking>{
-    return this.httpClient.delete<Booking>("http://localhost:3000/Bookings/" + id)
-  }
 }
